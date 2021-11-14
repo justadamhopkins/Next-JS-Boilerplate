4c7bb914cd0d1818989e5fa723acc69324464fda
justadamhopkins <adamhopkins87@gmail.com>
2021-11-14 22:13:25 +0000

    finish storybook setup

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,45 @@
+const customViewports = {
+  extraSmall: {
+    name: 'Extra Small Mobile',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+  small: {
+    name: 'Landscape Mobile',
+    styles: {
+      width: '640px',
+      height: '480px',
+    },
+  },
+  medium: {
+    name: 'Tablet',
+    styles: {
+      width: '960px',
+      height: '540px',
+    },
+  },
+  large: {
+    name: 'Laptop',
+    styles: {
+      width: '1280px',
+      height: '720px',
+    },
+  },
+  extraLarge: {
+    name: 'Desktop',
+    styles: {
+      width: '1440px',
+      height: '900px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   layout: 'centered',
+  viewport: { viewports: customViewports },
   controls: {
     matchers: {
       color: /(background|color)$/i,
