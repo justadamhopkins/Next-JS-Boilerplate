c821848964a1834149a62b3579388b18063c647f
justadamhopkins <adamhopkins87@gmail.com>
2022-10-09 21:57:05 +0100

    refactor: jest config

--- jest.config.js
+++ jest.config.js
@@ -38,18 +38,6 @@ module.exports = {
     'ts-jest': {
       isolatedModules: true,
       tsconfig: '<rootDir>/tsconfig.test.json',
-      astTransformers: {
-        before: [
-          {
-            path: '@formatjs/ts-transformer/ts-jest-integration',
-            options: {
-              // options
-              overrideIdFn: '[sha512:contenthash:base64:6]',
-              ast: true,
-            },
-          },
-        ],
-      },
     },
     __PATH_PREFIX__: ``,
     __BASE_PATH__: ``,
@@ -58,27 +46,28 @@ module.exports = {
   moduleDirectories: ['node_modules', '<rootDir>/src/utils/tests'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'd.ts', 'tsx', 'node'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/src',
+    prefix: '<rootDir>',
   }),
   clearMocks: true,
   preset: 'ts-jest',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>'],
   setupFiles: ['<rootDir>/configs/jest/jest.setup.ts'],
   setupFilesAfterEnv: [
     '<rootDir>/configs/jest/setup.postenv.ts',
-    '<rootDir>/configs/jest/loader-shim.js',
+    '<rootDir>/configs/jest/transformers/loader-shim.js',
   ],
-  setupFilesAfterEnv: ['<rootDir>/config/jest/jest.setup.postenv.ts'],
+  setupFilesAfterEnv: ['<rootDir>/configs/jest/setup.postenv.ts'],
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.ts?(x)$': 'ts-jest',
-    '^.+\\.tsx?$': '<rootDir>/config/jest/preprocess.js',
-    '^.+\\.json$': '<rootDir>/config/jest/json-transform.js',
+    '^.+\\.tsx?$': '<rootDir>/configs/jest/transformers/preprocess.js',
+    '^.+\\.json$': '<rootDir>/configs/jest/transformers/json-transform.js',
     '^(?!.*\\.(png|jpe?g|gif|svg)$)':
-      '<rootDir>/config/jest/image-transform.js',
-    '^(?!.*\\.(js|jsx|mjs)$)': '<rootDir>/config/jest/file-transform.js',
+      '<rootDir>/configs/jest/transformers/image-transform.js',
+    '^(?!.*\\.(js|jsx|mjs)$)':
+      '<rootDir>/configs/jest/transformers/file-transform.js',
   },
   transformIgnorePatterns: ['/node_modules/'],
 };
