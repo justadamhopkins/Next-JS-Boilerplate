e98a3601405d59f96129dc042fa5ff8dfec19f8c
justadamhopkins <adamhopkins87@gmail.com>
2022-10-10 22:37:30 +0100

    feat: added app container to _app tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
+import { GlobalStyles } from '@styles/global';
 import { AppProps } from 'next/app';
 import React from 'react';
 
+import AppContainer from '../components/shared/global/AppContainer';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </>
+  );
 }
 
 export default MyApp;
