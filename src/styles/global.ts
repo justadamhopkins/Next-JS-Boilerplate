a06eecd74a3acadd385247773a21bcd114f77b79
justadamhopkins <adamhopkins87@gmail.com>
2022-10-10 22:40:19 +0100

    feat: added streamlined global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { Typography } from '@styles/utilities/typography';
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+  }
+
+  ${normalize}
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: #1a202c;
+    font-size: 62.5%;
+  }
+
+  body {
+    line-height: 1.5;
+    letter-spacing: 0;
+    background-color: #f7fafc;
+    font-size: 1.6rem;
+  }
+
+  ${Typography}
+`;
