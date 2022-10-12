b08ec92a70dffd02054377290244c70e82b9823a
justadamhopkins <adamhopkins87@gmail.com>
2022-10-12 22:19:14 +0100

    feat: added heading styles

--- src/styles/utilities/typography.ts
+++ src/styles/utilities/typography.ts
@@ -1,4 +1,39 @@
+import { fontFamilys } from '@styles/utilities/fonts';
+import { EHeadingLevel } from 'common/types/enums/typography';
 import { css } from 'styled-components';
+
+export const headingStyles: Record<EHeadingLevel, ReturnType<typeof css>> = {
+  [EHeadingLevel.One]: css`
+    font-size: 4.209rem;
+    font-family: ${fontFamilys.nunitoSans};
+    font-weight: bold;
+    font-style: normal;
+  `,
+  [EHeadingLevel.Two]: css`
+    font-size: 3.157rem;
+    font-family: ${fontFamilys.nunitoSans};
+    font-weight: bold;
+    font-style: normal;
+  `,
+  [EHeadingLevel.Three]: css`
+    font-size: 2.369rem;
+    font-family: ${fontFamilys.nunitoSans};
+    font-weight: bold;
+    font-style: normal;
+  `,
+  [EHeadingLevel.Four]: css`
+    font-size: 1.777rem;
+    font-family: ${fontFamilys.nunitoSans};
+    font-weight: bold;
+    font-style: normal;
+  `,
+  [EHeadingLevel.Five]: css`
+    font-size: 1.333rem;
+    font-family: ${fontFamilys.nunitoSans};
+    font-weight: bold;
+    font-style: normal;
+  `,
+};
 
 export const Typography = css`
   h1,
@@ -9,5 +44,25 @@ export const Typography = css`
   h6 {
     margin: 0;
     line-height: 1.25;
+  }
+
+  h1 {
+    ${headingStyles.h1}
+  }
+
+  h2 {
+    ${headingStyles.h2}
+  }
+
+  h3 {
+    ${headingStyles.h3}
+  }
+
+  h4 {
+    ${headingStyles.h4}
+  }
+
+  h5 {
+    ${headingStyles.h5}
   }
 `;
