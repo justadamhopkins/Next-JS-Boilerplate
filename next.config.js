d3425e1673efc16ec1408a4561eb55395b389735
justadamhopkins <adamhopkins87@gmail.com>
2022-12-14 21:43:49 +0000

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 const webpack = require('webpack');
 const env = require('./configs');
+import path from 'path';
 
 const nextConfig = {
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: true,
   },
+
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin({ ...env }));
 
