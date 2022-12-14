c13f2cfc4f93992d09db0c8ecbe8e43b8de7510e
justadamhopkins <adamhopkins87@gmail.com>
2022-12-14 21:44:26 +0000

    feat: added global styles to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import React from 'react';
 
 import AppContainer from '../components/shared/global/AppContainer';
+import '../../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
