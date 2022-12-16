18583bf4206bbf00721989a5bda0f664bd425acf
justadamhopkins <adamhopkins87@gmail.com>
2022-12-16 15:10:24 +0000

    chore: remove styled components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,45 +1,7 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-import { ServerStyleSheet } from 'styled-components';
-
-import { RenderPageResult } from '../../next-server-utils';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<RenderPageResult> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      } as RenderPageResult;
-    } finally {
-      sheet.seal();
-    }
-  }
-
   render(): JSX.Element {
     const envKey = 'env-vars';
     return (
