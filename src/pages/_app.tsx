5b1b3d53ab430ddac2f90bde69e1432ec3df8a49
justadamhopkins <adamhopkins87@gmail.com>
2023-01-20 22:15:44 +0000

    feat: basic react query ssr integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,51 @@
 import { AppProps } from 'next/app';
 import React from 'react';
+import {
+  dehydrate,
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import AppContainer from '../components/shared/global/AppContainer';
-
 import '@styles/styles.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <AppContainer>
-        <Component {...pageProps} />
-      </AppContainer>
-    </>
-  );
-}
+type TCoreAppProps = AppProps & {
+  reactQueryProps: { dehydratedState: DehydratedState };
+};
 
-export default MyApp;
+const CoreApp = ({ Component, reactQueryProps, pageProps }: TCoreAppProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={reactQueryProps.dehydratedState}>
+        <AppContainer>
+          <Component {...pageProps} />
+        </AppContainer>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+CoreApp.getInitialProps = async ({ Component, ctx }) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery('posts', () => ({
+    foo: 'posts',
+  }));
+
+  return {
+    pageProps: Component.getServerSideProps
+      ? await Component.getServerSideProps(ctx)
+      : {},
+    reactQueryProps: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export default CoreApp;
