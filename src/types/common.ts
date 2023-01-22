448dc3ec576029a32393cd6579964cb288622c8d
justadamhopkins <adamhopkins87@gmail.com>
2023-01-22 14:21:08 +0000

    feat: added common name space

---
+++ src/types/common.ts
@@ -0,0 +1,24 @@
+import { AxiosInstance } from 'axios';
+
+import { EApiVersion, EHttpMethod } from '@typings/enums/api';
+
+export declare namespace Common {
+  interface IHttpRequest<Body = any> {
+    type: string | string[];
+    method: EHttpMethod;
+    body?: Body;
+    headers?: IHttpHeaders;
+    client: AxiosInstance;
+    url: (options: IHttpRequestUrl) => string;
+    version?: EApiVersion;
+  }
+
+  interface IHttpRequestUrl {
+    version?: EApiVersion;
+    url: (options: IHttpRequestUrl) => string;
+  }
+
+  interface IHttpHeaders {
+    [headerKey: string]: string;
+  }
+}
---
+++ src/types/enums/api.ts
@@ -0,0 +1,11 @@
+export enum EApiVersion {
+  one = 'v1',
+}
+
+export enum EHttpMethod {
+  Get = 'GET',
+  Post = 'POST',
+  Put = 'PUT',
+  Patch = 'PATCH',
+  Delete = 'DELETE',
+}
