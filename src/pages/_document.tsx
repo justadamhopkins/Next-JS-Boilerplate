2f341a71d81a22c5588e2d58a45add28b624023e
justadamhopkins <adamhopkins87@gmail.com>
2023-01-28 21:58:31 +0000

    chore: remove react env

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,17 +3,9 @@ import React from 'react';
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
-    const envKey = 'env-vars';
     return (
       <Html>
-        <Head>
-          <script
-            id={envKey}
-            key={envKey}
-            data-testid={envKey}
-            src={'/__ENV.js'}
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
