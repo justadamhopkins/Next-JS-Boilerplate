e3e49d6946fc154b9a7197b2e9d24762d9ecbffd
Adam Hopkins <adamhopkins87@gmail.com>
2023-09-18 22:49:47 +0100

    chore: moved page dir

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>new app</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+        molestiae, nobis. Ad aliquam aspernatur dolores harum ipsa omnis
+        possimus quo rerum vero. Dolore dolorem neque numquam perspiciatis
+        provident rerum voluptatum?
+      </p>
+    </>
+  );
+};
+
+export default HomePage;
