4156c9258599f3243cffbde9d154cdc6d74c36ed
Adam Hopkins <adamhopkins87@gmail.com>
2023-10-05 22:47:52 +0100

    refactor: css to use new module setup

--- src/components/_core/AppContainer/AppContainer.tsx
+++ src/components/_core/AppContainer/AppContainer.tsx
@@ -1,10 +1,10 @@
-import classNames from 'classnames';
+import clsx from 'clsx';
 import React, { FC, ReactNode } from 'react';
 
-import styles from './AppContainer.module.scss';
+import styles from './AppContainer.module.css';
 
 const AppContainer: FC<{ children: ReactNode }> = ({ children }) => {
-  return <main className={classNames(styles.test)}>{children}</main>;
+  return <main className={clsx(styles.test)}>{children}</main>;
 };
 
 export default AppContainer;
