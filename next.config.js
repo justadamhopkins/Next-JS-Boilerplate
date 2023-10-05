bf9cda04623edc6fb21c2d440e1e3021ad5aec94
Adam Hopkins <adamhopkins87@gmail.com>
2023-10-05 22:48:14 +0100

    chore: remove next config func

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-const webpack = require('webpack');
-const aliases = require('./configs/webpack/aliases');
-const path = require('path');
-
-const nextConfig = {
-  swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
-    additionalData: '@use "@sass/core" as *;',
-  },
-  webpack(config) {
-    config.resolve.alias = Object.assign(config.resolve.alias, aliases);
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
