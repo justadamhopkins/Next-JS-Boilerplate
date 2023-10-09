73be58f65b4b5a1e0946ec7118cc75459019983b
Adam Hopkins <adamhopkins87@gmail.com>
2023-10-09 23:05:29 +0100

    refactor: vitest setup

--- configs/vitest/setupTests.ts
+++ configs/vitest/setupTests.ts
@@ -1,7 +1,5 @@
 import '@testing-library/jest-dom/vitest';
-import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
 
-afterEach(() => {
-  cleanup();
-});
+process.env = {
+  ...process.env,
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,54 @@
+import path from 'path';
+
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+const testConfig = defineConfig({
   test: {
     globals: true,
+    clearMocks: true,
     include: ['**/*.test.{ts,tsx}'],
     environment: 'jsdom',
     setupFiles: ['./configs/vitest/setupTests.ts'],
+    alias: {
+      '@atoms/*': path.resolve(__dirname, 'src/components/_atoms'),
+      '@molecules/*': path.resolve(__dirname, 'src/components/_molecules/*'),
+      '@organisms/*': path.resolve(__dirname, 'src/components/_organisms'),
+      '@core/*': path.resolve(__dirname, 'src/components/_core'),
+      '@templates/*': path.resolve(__dirname, 'src/components/_templates'),
+      '@hooks/*': path.resolve(__dirname, 'src/components/_hooks'),
+      '@styles/*': path.resolve(__dirname, 'src/styles'),
+      '@common/*': path.resolve(__dirname, 'src/shared'),
+      '@typings/*': path.resolve(__dirname, 'src/types'),
+      'test-utils/*': path.resolve(__dirname, 'src/utils/tests'),
+    },
+    coverage: {
+      provider: 'istanbul',
+      enabled: true,
+      reportsDirectory: 'testCoverage',
+      reporter: ['json', 'text', 'lcov', 'clover'],
+      include: [
+        'src/**/*.{ts,tsx,}',
+        'server/**/*.{ts,}',
+        '!**/stories.{tsx,}',
+        '!**/*.stories.{tsx,}',
+        '!**/*.d.{ts,}',
+        '!**/test.{ts,tsx,}',
+        '!**/spec.{ts,tsx,}',
+        '!**/*.test.{ts,tsx,}',
+        '!**/*.spec.{ts,tsx,}',
+        '!src/**/__test__/**/*',
+        '!src/**/__test__/*',
+        '!src/**/__tests__/**/*',
+        '!src/**/__tests__/*',
+        '!src/**/__mock__/**/*',
+        '!src/**/__mock__/*',
+        '!src/**/__mocks__/*',
+        '!src/**/__mocks__/**/*',
+        '!scripts/**/*',
+      ],
+      exclude: ['/node_modules/', './configs/vitest/setupTests.ts'],
+    },
   },
 });
+
+export default testConfig;
