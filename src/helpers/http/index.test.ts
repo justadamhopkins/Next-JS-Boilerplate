c284edd75eaec2861d141580c6efc19098e3b109
Adam Hopkins <adamhopkins87@gmail.com>
2023-10-16 21:11:27 +0100

    feat: added ts shoe horn

--- src/helpers/http/index.test.ts
+++ src/helpers/http/index.test.ts
@@ -1,3 +1,4 @@
+import { fromPartial } from '@total-typescript/shoehorn';
 import { vi } from 'vitest';
 
 import { Common } from '@typings/common';
@@ -5,74 +6,90 @@ import { EHttpMethod } from '@typings/enums/api';
 
 import http from './index';
 
+vi.mock('axios');
+
 const mockurl = `www.mockapi;com`;
+
 const mockReqObj: Common.IHttpRequest<any> = {
   type: 'MOCK_REQ',
   method: EHttpMethod.Get,
   url: () => `www.mockapi;com`,
-  client: {
+  client: fromPartial({
     request: vi.fn(() =>
       Promise.resolve({
         data: 'server says hello!',
         status: 200,
       }),
     ),
-  },
+  }),
 };
 
-vi.mock('axios');
-
-describe('http.send.name', () => {
-  afterEach(() => {
-    // mockAxios.reset();
-  });
-
+describe(http.send.name, () => {
   describe('when making a req', () => {
     it('should fetch data successfully', async () => {
-      const result = http.send(mockReqObj);
+      const spy = vi.fn(() =>
+        Promise.resolve({
+          data: 'server says hello!',
+          status: 200,
+        }),
+      );
+
+      const result = http.send({
+        ...mockReqObj,
+        client: fromPartial({ request: spy }),
+      });
 
       await expect(result).resolves.toEqual('server says hello!');
 
-      // expect(mockAxios.request).toHaveBeenCalledWith({
-      //   url: mockurl,
-      //   data: undefined,
-      //   headers: {},
-      //   method: 'GET',
-      // });
+      expect(spy).toHaveBeenCalledWith({
+        url: mockurl,
+        data: undefined,
+        headers: {},
+        method: 'GET',
+      });
     });
-    // describe('when something goes wrong', () => {
-    //   describe('and should throw is true', () => {
-    //     it('should catch and rethrow the error', async () => {
-    //       const result = http.send(mockReqObj);
-    //
-    //       mockAxios.mockError(new Error('BORKED'));
-    //
-    //       await expect(result).rejects.toThrowError('BORKED');
-    //
-    //       expect(mockAxios.request).toHaveBeenCalledWith({
-    //         url: mockurl,
-    //         data: undefined,
-    //         headers: {},
-    //         method: 'GET',
-    //       });
-    //     });
-    //   });
-    //   describe('and should throw is false', () => {
-    //     it('should catch the error and fail gracefully', async () => {
-    //       const result = http.send(mockReqObj, { shouldThrow: false });
-    //
-    //       mockAxios.mockError(new Error('BORKED'));
-    //
-    //       await expect(result).resolves.toBeUndefined();
-    //
-    //       expect(mockAxios.request).toHaveBeenCalledWith({
-    //         url: mockurl,
-    //         data: undefined,
-    //         headers: {},
-    //         method: 'GET',
-    //       });
-    //     });
-    //   });
-    // });
+    describe('when something goes wrong', () => {
+      describe('and should throw is true', () => {
+        it('should catch and rethrow the error', async () => {
+          const spy = vi.fn(() => Promise.reject('BORKED'));
+
+          const result = http.send({
+            ...mockReqObj,
+            client: fromPartial({ request: spy }),
+          });
+
+          await expect(result).rejects.toThrowError('BORKED');
+
+          expect(spy).toHaveBeenCalledWith({
+            url: mockurl,
+            data: undefined,
+            headers: {},
+            method: 'GET',
+          });
+        });
+      });
+      describe('and should throw is false', () => {
+        it('should catch the error and fail gracefully', async () => {
+          const spy = vi.fn(() => Promise.reject('BORKED'));
+
+          const result = http.send(
+            {
+              ...mockReqObj,
+              client: fromPartial({ request: spy }),
+            },
+            { shouldThrow: false },
+          );
+
+          await expect(result).resolves.toBeUndefined();
+
+          expect(spy).toHaveBeenCalledWith({
+            url: mockurl,
+            data: undefined,
+            headers: {},
+            method: 'GET',
+          });
+        });
+      });
+    });
   });
 });
